a0e3b401aa517c435b519df9226c6445ccf6e361
Никита Молостов <n@memolostov.ru>
2025-04-07 20:23:30 +0300

    homework 9.1

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+def get_mask_card_number(card_number: str) -> str:
+    """ Функция принимает на вход номер карты в виде числа
+    и возвращает маску номера по правилу XXXX XX** **** XXXX
+    """
+    cleaned_number = card_number.replace(" ", "")
+    first_part = cleaned_number[:6]
+    last_part = cleaned_number[-4:]
+    hidden_part = "** ****"
+    masked_number = f"{first_part[:4]} {first_part[4:6]}{hidden_part} {last_part}"
+    return masked_number
+
+
+def get_mask_account(account: str) -> str:
+    """" Функция принимает на вход номер счета в виде числа
+    и возвращает маску номера по правилу **XXXX
+    """
+    cleaned_account = account.replace(" ", "")
+    last_part_account = cleaned_account[-4:]
+    hidden_part_account = "**"
+    masked_account = f"{hidden_part_account}{last_part_account}"
+    return masked_account
