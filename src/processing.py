d614deda788e477878cd7ccb47b0f70810f15a66
Никита Молостов <n@memolostov.ru>
2025-04-15 01:36:10 +0300

    processing done

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def filter_by_state(operations:list[dict], state = 'EXECUTED') -> list[dict]:
+    """ Фильтрует список словарей, оставляя только те, у которых ключ 'state'
+       равен заданному значению """
+    result_operation = []
+    for operation in operations:
+        if operation.get('state') == state:
+            result_operation.append(operation)
+    return result_operation
+
+
+def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
+    """ Сортирует список словарей по дате (ключ 'date') в заданном порядке """
+    return sorted(operations, key=lambda operation: operation.get('date'), reverse=reverse)
