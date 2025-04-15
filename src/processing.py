57951389414b98ea48a09e9878c3f2677d91d8a7
Никита Молостов <n@memolostov.ru>
2025-04-15 18:28:45 +0300

    homework 10_1 fixed_1

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,13 @@
-def filter_by_state(operations:list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """ Фильтрует список словарей, оставляя только те, у которых ключ 'state'
        равен заданному значению """
-    result_operation = []
-    for operation in operations:
-        if operation.get('state') == state:
-            result_operation.append(operation)
-    return result_operation
+    result_transaction = []
+    for transaction in transactions:
+        if transaction.get('state') == state:
+            result_transaction.append(transaction)
+    return result_transaction
 
 
-def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
     """ Сортирует список словарей по дате (ключ 'date') в заданном порядке """
-    return sorted(operations, key=lambda operation: operation.get('date'), reverse= reverse)
+    return sorted(transactions, key=lambda transaction: transaction['date'], reverse=reverse)
