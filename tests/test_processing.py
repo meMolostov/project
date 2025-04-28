83e513268aac4d894490eb4ada158caa740f4660
Никита Молостов <n@memolostov.ru>
2025-04-29 02:04:25 +0300

    homework 10_1 fixed_1

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import pytest
 
 from src.processing import filter_by_state, sort_by_date
@@ -39,8 +37,9 @@ def test_sort_by_date_default(date_list):
 
 def test_sort_by_date_increase(date_list):
     """Тестирование фильтрации по возрастанию date"""
-    assert sort_by_date(date_list,False) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                       {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                       ]
+    assert sort_by_date(date_list, False) == [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ]
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,9 +1,8 @@
-import pytest
-
 from src.widget import mask_account_card, get_date
 
 
 def test_mask_account_card():
+    """Базовая проверка"""
     assert mask_account_card('Visa Platinum 7000792289606361') == 'Visa Platinum 7000 79** **** 6361'
     assert mask_account_card('Счет 73654108430135874305') == 'Счет **4305'
     assert mask_account_card('Мир 19841984') == 'Введите корректные данные'
@@ -11,5 +10,6 @@ def test_mask_account_card():
 
 
 def test_get_date():
+    """Базовая проверка"""
     assert get_date('2024-03-11T02:26:18.671407') == '11.03.2024'
     assert get_date('') == 'Введите корректные данные'
